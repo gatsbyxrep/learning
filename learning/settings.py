6758c5b426ac4a768d50be958f719751ce4c30e1
russiancplusplus <gatsbynalda@gmail.com>
2021-12-14 14:34:04 +0300

    Lab 6

--- learning/settings.py
+++ learning/settings.py
@@ -61,6 +61,7 @@ TEMPLATES = [
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
+                'django.template.context_processors.media',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
@@ -124,3 +125,12 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = BASE_DIR / 'mediafiles/'
+MEDIA_URL = '/media/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+LOGIN_REDIRECT_URL = 'stock:account'
+LOGIN_URL = 'login'
